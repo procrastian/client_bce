f6f7ea13946ad12a671ba904789b67c992a82257
Your Name <your_email@address>
2023-08-23 19:10:32 +0100

    Components moved to Home Page from App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import './App.css';
 import './globalStylingVars.css'
-import { Header, Qualifications, Footer } from './Components';
+import './App.css';
 import Home from './pages/home'
-import About from './pages/about'
-import CourseList from './pages/course_list'
-import ContactForm from './pages/contact_form';
 
 function App() {
   return (
     <>
-      <Header />
       <Home />
-      <About />
-      <CourseList />
-      <ContactForm />
-      <Qualifications />
-      <Footer />
     </>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,23 @@
+
+import { Header, Qualifications, Footer } from '../../Components';
+import About from '../about'
+import CourseList from '../course_list'
+import ContactForm from '../contact_form';
+
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Welcome to BCE</h1>
-        <h2>This is the Home Component</h2>
-      </div>
+      <Header />
+        <div id="home">
+          <h1>Welcome to BCE</h1>
+          <h2>This is the Home Page</h2>
+          <div class="img"></div>
+        </div>
+        <About />
+        <CourseList />
+        <ContactForm />
+        <Qualifications />
+      <Footer />
     </>
   );
 }
