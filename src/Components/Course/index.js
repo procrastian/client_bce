dd02e69ebd0ae7306c6140e2ea956a5af4848957
Your Name <your_email@address>
2023-08-26 13:54:24 +0100

    course list page started

---
+++ src/Components/Course/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export default function Course() {
+  return (
+    <>
+      <div className="course">
+        <h1>This is A Course</h1>
+      </div>
+    </>
+  );
+}
--- src/pages/course_list/index.js
+++ src/pages/course_list/index.js
@@ -1,8 +1,24 @@
+import "./style.css";
+import { useState } from "react";
+
 export default function CourseList() {
+  const [coursesState, setCoursesState] = useState([]);
+
   return (
     <>
-      <div className="card" id="course_list">
+      <div className="card" id="course_list_card">
         <h1>This is the CourseList</h1>
+        <ul className="course_list">
+          {coursesState.map((item, index) => (
+            <li key={index}>
+              <div className="course_info">
+                <img src="" alt="" />
+                <p></p>
+              </div>
+              {/* <AddToCart id={`${item.id}`} coursesState={coursesState} cartState={cartState} setCartState={setCartState}/> */}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
