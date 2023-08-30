d175c8d2bfb7f5f725f028718490c491eea9d935
Your Name <your_email@address>
2023-08-30 19:12:19 +0100

    footer styled with placeholders

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,7 +3,17 @@ import "./style.css";
 export default function Footer() {
   return (
     <div className="component" id="footer">
-
+      <div id="flash">
+      </div>
+      <div id="copyright">
+        <p>© Base Camp Education 2023</p>
+      </div>
+      <div id="icons">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+      </div>
     </div>
   );
 }
