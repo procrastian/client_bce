5ac72a24430a29bfd46299b3dda1065abeb780d4
Your Name <your_email@address>
2023-08-30 18:36:35 +0100

    header styles and images added

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -2,8 +2,8 @@ import "./style.css";
 
 export default function Footer() {
   return (
-    <div className="component" id="header">
-      <p>This is the Footer Component</p>
+    <div className="component" id="footer">
+
     </div>
   );
 }
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,17 @@
+import logo from "../../assets/BCP_LOGO_FINAL_SVG.svg";
+import menu from "../../assets/Menu_Weight_Regular.svg";
 import "./style.css";
 
 export default function Header() {
   return (
     <div className="component" id="header">
-      <p>This is the Header Component</p>
+      <div id="logo">
+        <img src={logo} alt="BCE logo" />
+      </div>
+      <div id="buffer"></div>
+      <div id="menu">
+        <img src={menu} alt="Menu icon" />
+      </div>
     </div>
-  )
+  );
 }
