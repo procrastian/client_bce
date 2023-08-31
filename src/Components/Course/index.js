bb64827378de0d21ac52f3e6689107e86018b018
Your Name <your_email@address>
2023-08-31 20:06:46 +0100

    course cards now display required course details

--- src/Components/Course/index.js
+++ src/Components/Course/index.js
@@ -2,8 +2,18 @@ import "./style.css";
 
 export default function Course({ course }) {
   return (
-    <li>
+    <li className="course_card">
+      <h3>{course.title}</h3>
+      <img src={course.coverImage} alt={`${course.title} cover`}/>
+      <p>{course.description}</p>
+      <p>This course lasts for {course.length} sessions</p>
+      <p>The next course starts on: {course.startDate}</p>
+      <h4>Tags</h4>
+      <ul> {course.tags.map((tag) => {
+        return <li>{tag}</li>
+      })}
 
+      </ul>
       <button>Find out More</button>
     </li>
   );
